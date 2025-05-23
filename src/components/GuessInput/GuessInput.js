057387e02896e94cf64aab5ae87a7bd814feb754
Mustafa Ozturk <mozturkpro@gmail.com>
2025-05-23 18:21:23 -0400

    ex1

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+function GuessInput() {
+  const [guess, setGuess] = React.useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log({ guess });
+    setGuess("");
+  }
+
+  return (
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        required
+        minLength={5}
+        maxLength={5}
+        pattern="[a-zA-Z]{5}"
+        title="5 letter word"
+        id="guess-input"
+        type="text"
+        value={guess}
+        onChange={(e) => {
+          const nextGuess = e.target.value.toUpperCase();
+          setGuess(nextGuess);
+        }}
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
